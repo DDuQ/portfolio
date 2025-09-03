ea8c61b73f6e04d35467391af31fe70c60551d58
DDuQ <61994091+DDuQ@users.noreply.github.com>
2025-09-03 18:12:16 -0500

    Publish portfolio.

--- Models/Project.cs
+++ Models/Project.cs
@@ -6,7 +6,7 @@ public class Project
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string ImageUrl { get; set; } = string.Empty;
-    public string[] Technologies { get; set; } = Array.Empty<string>();
+    public string[] Technologies { get; set; } = [];
     public string Category { get; set; } = string.Empty;
     public string? ProjectUrl { get; set; }
     public string? GithubUrl { get; set; }
